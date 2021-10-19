21ed4a5dbc10f9c51361ad134f51a916bd518363
Eldar <manny2002@mail.ru>
2021-10-19 20:28:12 +0500

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,11 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reducer from './rootReducer.js';
 import rootSaga from './rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(
-        applyMiddleware(
-            sagaMiddleware,
-        )
-    )
-);  
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
