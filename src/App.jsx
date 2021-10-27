fc7fc26968abec29878f01c7f03ad261571402db
Eldar <manny2002@mail.ru>
2021-10-27 19:20:53 +0500

    Some fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import ExercisePage from './components/exercise';
 export default function App() {
   return (
     <Router>
-        <Switch>
-          <Route path="/courses/:courseId/exercises/:exerciseId">
-            <ExercisePage />
-          </Route>
-        </Switch>
+      <Switch>
+        <Route path="/courses/:courseId/exercises/:exerciseId">
+          <ExercisePage />
+        </Route>
+        <Route path="/users">
+          <h1>Users</h1>
+        </Route>
+        <Route path="/">
+          <h1>Main</h1>
+        </Route>
+      </Switch>
     </Router>
   );
 }
