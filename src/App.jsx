d3f504eafbeab2a39d5c4879c3fa76d247f45b10
Eldar <manny2002@mail.ru>
2021-10-28 00:53:26 +0500

    Make it better

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,12 @@ import ExercisePage from './components/exercise';
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/courses/python-for-beginners/exercises/1">Courses</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-          <Route exect path="/courses/:courseId/exercises/:exerciseId">
-            <ExercisePage />
-          </Route>
-          <Route exect path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route exect path="/">
-            <h1>Main</h1>
-          </Route>
-      </div>
+      <Route exect path="/courses/:courseId/exercises/:exerciseId">
+        <ExercisePage />
+      </Route>
+      <Route exect path="/">
+        <Link to="/courses/python-for-beginners/exercises/1">Упражнение</Link>
+      </Route>
     </Router>
   );
 }
