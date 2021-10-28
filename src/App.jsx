c237034375a6f30fd1a36d35b4f1e5f8043422a5
Eldar <manny2002@mail.ru>
2021-10-28 23:48:46 +0500

    React env test

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import ExercisePage from './components/exercise';
 export default function App() {
   return (
     <Router>
-      <Route exect path="/courses/:courseId/exercises/:exerciseId">
+      <Route exact path="/courses/:courseId/exercises/:exerciseId">
         <ExercisePage />
+        {console.log('react', process.env)}
       </Route>
     </Router>
   );
