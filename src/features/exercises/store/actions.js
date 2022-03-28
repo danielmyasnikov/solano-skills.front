a7fa38ff6060e817601a3ea3418a3003222f5b79
Даниил Мясников <daniil.myasnikov@rt.ru>
2022-03-28 12:57:56 +1100

    added package

--- src/features/exercises/store/actions.js
+++ src/features/exercises/store/actions.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import { Api } from '@src/api/Api';
+import { Api } from 'src/api/Api';
 
 export const getExerciseById = createAsyncThunk(
   'exercises/getExerciseById',
