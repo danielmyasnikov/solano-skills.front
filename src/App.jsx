8fd84ece1b70afb3e9f87e95352ed5dc638e86f8
Ilya <IlVPlatonov@beeline.ru>
2022-04-02 11:53:43 +0300

    правки

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,9 @@ function App() {
           ))}
           <Route component={NotFoundPage} />
         </Switch>
-        {process.env.NODE_ENV === 'production' && <YMInitializer accounts={[process.env.ID]} />}
+        {process.env.NODE_ENV === 'production' && (
+          <YMInitializer accounts={[process.env.REACT_APP_SEO_YM_ID]} />
+        )}
       </div>
     </ThemeProvider>
   );
