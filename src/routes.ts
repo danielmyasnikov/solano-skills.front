775a92fa2232304d2c6ecc7fa880644661bd4d67
Ilya <IlVPlatonov@beeline.ru>
2022-04-04 18:10:02 +0300

    WIP: About page

--- src/routes.ts
+++ src/routes.ts
@@ -24,6 +24,7 @@ import { Instructors } from '@src/features/instructors';
 
 import { FailPaymentPage } from '@src/features/payment/Fail';
 import { ElementType } from 'react';
+import { AboutPage } from '@components/landings/AboutPage';
 
 export const routes: {
   path: string;
@@ -54,6 +55,12 @@ export const routes: {
     path: '/courses',
     Component: CoursesPage,
     wrap: true,
+    exact: true,
+  },
+  {
+    path: '/about',
+    Component: AboutPage,
+    wrap: false,
     exact: true,
   },
   {
